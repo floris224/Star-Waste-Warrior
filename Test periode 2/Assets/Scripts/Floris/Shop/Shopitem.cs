9760e3ffa42de4550bb012d8b205ea66c537d1be
Florisvdk <floris_2@hotmail.nl>
2023-05-17 21:10:46 +0200

    shop ui + Begin Scriptable Objects

---
+++ Test periode 2/Assets/Scripts/Floris/Shop/Shopitem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "shopmenu", menuName = "Scriptable Objects /New shop item",order = 1)]
+public class Shopitem : ScriptableObject
+{
+    public string title;
+    public string description;
+    public int cost;
+}
